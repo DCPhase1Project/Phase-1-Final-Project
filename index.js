baacd76a401eb663aab11f1d8469137639ef0500
Nick Phelps <Nick90052@Gmail.com>
2018-11-15 21:18:52 -0600

    Added Rendered Images

--- index.js
+++ index.js
@@ -17,20 +17,45 @@ function submitSearch () {
     }
     
     $.ajax(requestObj)
-        .done(function (response) {
+        .then(function (response) {
             console.log('response = ', response)
             console.log(response.businesses[0].name)
             returnData = response
             return returnData
         }) 
-        returnData = response.businesses
+        .then(renderRestaurant)
+        .then(renderFinal)
         console.log(returnData)
+
+
 }//submit Search
 
+function renderRestaurant (restaurant) {
+    console.log('this is my Restaurant Data', restaurant)
+    let restaurantHTML = restaurant.businesses.map(function (currentRestaurant) {
+        console.log(currentRestaurant)
+        let restaurantHTMLString = `
+            <div class="card bg-dark text-white">
+                <img class="card-img-top" src="${currentRestaurant.image_url}" alt="${currentRestaurant.name}">
+                <div class="card-body">
+                    <div class="row">
+                        <h5 class="card-title">${currentRestaurant.name}</h5>
+                    </div>
+                    <button onclick="saveToRestaurantList('${currentRestaurant.id}')" type="button" class="btn btn-primary btn-lg btn-block">Add to Restaurant list</button>
+                </div>
+            </div>
+        `
+        return restaurantHTMLString
+    })//map function
+    console.log(restaurantHTML.join(''))
+    return restaurantHTML.join('')
+}//renderRestaurant
 
+function renderFinal (htmlString) {
+    $('.container').html('<div class="card-columns">' + htmlString + '</div>')
 
+}
 
-// function renderRestaurant (restaurant) {
-//     let restaurantHTML = 
+function saveToRestaurantList () {
 
-// }//renderRestaurant
+} //saveToRestaurantList
