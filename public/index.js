f415c59712f97a528cd8643cb94217415cb4c1af
fatherofbread <ahmedabuelaish@hotmail.com>
2018-11-17 18:09:33 -0600

    working indexJS v2.0

--- public/index.js
+++ public/index.js
@@ -1,35 +1,21 @@
-let token = 'Bearer aiGs24xZ8kMmDUOG_HpUhfizqZuFgS2bOUmTt-SudUenzhKIWxJsn6ooKpWBzy7KTE9qs90W4Tw15Jau3bbhgTCa2n3-AMBugVl6ChhBRpjxCv-OQNNyjXvlI9LsW3Yx'
-let yelpSearchURL = 'https://api.yelp.com/v3/businesses/search'
-let corsHelper = 'https://cors-anywhere.herokuapp.com'
+const token = 'Bearer aiGs24xZ8kMmDUOG_HpUhfizqZuFgS2bOUmTt-SudUenzhKIWxJsn6ooKpWBzy7KTE9qs90W4Tw15Jau3bbhgTCa2n3-AMBugVl6ChhBRpjxCv-OQNNyjXvlI9LsW3Yx'
+const yelpSearchURL = 'https://api.yelp.com/v3/businesses/search'
+const corsHelper = 'https://cors-anywhere.herokuapp.com'
 let returnData = ''
+
+const defaultSearch = 'food'
+let searchTerm = defaultSearch // initial search term
 
 let currentLat = 29.752948 // values from Derrick
 let currentLong = -95.339069 // values from Derrick
-
-console.log('in index.js')
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Event Listeners
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 document.addEventListener('DOMContentLoaded', function () {
-  let requestObj = {
-    'url': corsHelper + '/' + yelpSearchURL,
-    'data': { term: 'food', latitude: currentLat, longitude: currentLong },
-    headers: { 'Authorization': token },
-    error: function (jqXHR, testStatus, errorThrown) {
-      console.log('Ajax error, jqXHR = ', jqXHR, ', testStatus = ', testStatus, ', errorThrown = ', errorThrown)
-    }
-  }// request obj
-
-  $.ajax(requestObj)
-    .then(function (response) {
-      console.log('response = ', response)
-      console.log(response.businesses[0].name)
-      returnData = response
-      return returnData
-    })
-    .then(giveDerrickObject)
+  console.log('initializing index.js v2.0')
+  requestResponseObject()
 })// DOMContentLoaded
 
 document.getElementById('searchButton').addEventListener('click', function (evt) {
@@ -42,34 +28,56 @@ document.getElementById('search-form').addEventListener('submit', function (evt)
   submitSearch()
 })
 
-function submitSearch () {
-  let restaurantSearch = document.getElementById('search-bar').value
-  console.log(restaurantSearch)
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Create Response Object
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+function requestResponseObject () {
+  // if there's no search value then default search value
+  if (document.getElementById('search-bar').value) {
+    searchTerm = document.getElementById('search-bar').value
+  } else {
+    searchTerm = defaultSearch
+  }
+  // create requestObj
   let requestObj = {
     'url': corsHelper + '/' + yelpSearchURL,
-    'data': { term: restaurantSearch, location: '77006' },
+    'data': { term: searchTerm, latitude: currentLat, longitude: currentLong, categories: 'food' },
     headers: { 'Authorization': token },
     error: function (jqXHR, testStatus, errorThrown) {
       console.log('Ajax error, jqXHR = ', jqXHR, ', testStatus = ', testStatus, ', errorThrown = ', errorThrown)
     }
   }
-
+  // ajax request the object
   $.ajax(requestObj)
     .then(function (response) {
-      console.log('response = ', response)
-      console.log(response.businesses[0].name)
+      // console.log('response = ', response)
+      // console.log(response.businesses[0].name)
       returnData = response
       return returnData
     })
+    // .then(giveDerrickObject)
     .then(renderRestaurant)
     .then(renderFinal)
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Search Functionality
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function submitSearch () {
+  let restaurantSearch = document.getElementById('search-bar').value
+  console.log(restaurantSearch)
+  requestResponseObject()
 }// submit Search
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Rendering
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 function renderRestaurant (restaurant) {
-  console.log('this is my Restaurant Data', restaurant)
+  // console.log('this is my Restaurant Data', restaurant)
   let restaurantHTML = restaurant.businesses.map(function (currentRestaurant) {
-    console.log(currentRestaurant)
+    // console.log(currentRestaurant)
     let restaurantHTMLString = `
             <div class="card bg-dark text-white">
                 <img class="card-img-top" src="${currentRestaurant.image_url}" alt="${currentRestaurant.name}">
@@ -83,17 +91,17 @@ function renderRestaurant (restaurant) {
         `
     return restaurantHTMLString
   })// map function
-  console.log(restaurantHTML.join(''))
+
   return restaurantHTML.join('')
 }// renderRestaurant
 
 function renderFinal (htmlString) {
-  $('.renderingContainer').append('<div class="card-columns">' + htmlString + '</div>')
+  document.getElementById('restaurant-container').innerHTML = '<div class="card-columns">' + htmlString + '</div>'
 }
 
 function saveToRestaurantList () {
-
-} // saveToRestaurantList
+  // todo: save restaurants to list
+}
 
 function giveDerrickObject (responseData) {
 // console.log(responseData.businesses.name)
