1efe009934afb692d27c65174b80f61b9756c084
Chiza Barber <chiza.alba@gmail.com>
2018-11-20 04:26:27 -0600

    Moved Firebase init script into its own firebase.js file

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDZOaVFZL8mufS3bMoyoFZGUJK6Nob5bpw",
+    authDomain: "phase-1-final-project.firebaseapp.com",
+    databaseURL: "https://phase-1-final-project.firebaseio.com",
+    projectId: "phase-1-final-project",
+    storageBucket: "",
+    messagingSenderId: "835794979231"
+};
+firebase.initializeApp(config);
+
