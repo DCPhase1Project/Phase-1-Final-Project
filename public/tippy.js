285c8c310d4879b63a0a5083187a43fb3546fbb0
Chiza Barber <chiza.alba@gmail.com>
2018-12-03 16:01:04 -0600

    added functional tooltips via JavaScript library tippy.js

--- public/tippy.js
+++ public/tippy.js
@@ -3,9 +3,37 @@
 
 
 tippy (document.getElementById('login-button'), {
-    content: "I'm a tooltip!",
+    content: "Log in to see your favorite restaurants and the restaurants you want to visit!",
     delay: 100,
     arrow: true,
     arrowType: 'round',
+    size: 'large',
+    animation: 'scale',
+})
+
+tippy (document.getElementById('new-acct-button'), {
+    content: "Sign up to access additional features, like saving your favorite restaurants!",
+    delay: 100,
+    arrow: true,
+    arrowType: 'round',
+    size: 'large',
+    animation: 'scale',
+})
+
+tippy (document.getElementById('mapSection'), {
+    content: "Click the markers for more information on each restaurant",
+    delay: 100,
+    arrow: true,
+    arrowType: 'round',
+    size: 'large',
+    animation: 'scale',
+})
+
+tippy (document.getElementById('search-bar'), {
+    content: "You can search by restaurant name or cuisine type",
+    delay: 100,
+    arrow: true,
+    arrowType: 'round',
+    size: 'large',
     animation: 'scale',
 })
